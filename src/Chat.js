2efb27beb88ec2bb7cab8ddd75f54661a53ae345
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-18 21:54:47 +0530

    Chat header start

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Chat.css'
+import {IconButton, Avatar} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 const Chat = () => {
   return (
     <div className='chat'>
-      Chat Component
+      <div className='chat__header'>
+          <Avatar />
+          <div className='chat__headerInfo'>
+            <h3>Room Name</h3>
+            <p>Last Message...</p>
+          </div>
+          <div className='chat__herderRight'>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <IconButton>
+              <AttachFileIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+      </div>
+
     </div>
   )
 }
