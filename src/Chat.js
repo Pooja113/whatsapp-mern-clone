cc2c5ae62c05e453be9f807236f5a56cf30cb69d
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-19 10:04:56 +0530

    chat header and body

--- src/Chat.js
+++ src/Chat.js
@@ -4,7 +4,8 @@ import {IconButton, Avatar} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
 
 const Chat = () => {
   return (
@@ -28,14 +29,30 @@ const Chat = () => {
           </div>
       </div>
       <div className='chat__body'>
-        <p>
+        <p className='chat__message'>
           <span className='chat__name'>Pooja</span>
           message
           <span className='chat__timestamp'>{new Date().toUTCString()}</span>
         </p>
+        <p className='chat__message chat__reciever'>
+        <span className='chat__name'>Pooja</span>
+        message
+        <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+      </p>
       </div>
       <div className='chat__footer'>
-        <p></p>
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <form>
+          <input placeholder='Type a message' type="text" />
+          <button type='submit'></button>
+        </form>
+        
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+
       </div>
     </div>
   )
